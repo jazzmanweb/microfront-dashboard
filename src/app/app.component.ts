33f9a36b03e1465df316771342844e74a95acd24
Maciej Zoltowski <maciej.zoltowski@e-qsg.com>
2019-03-08 11:29:00 +0100

    v1.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import listOfHeros from 'microfront-common/heros.json';
+import {HeroModel} from 'microfront-common/model/hero.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'microfront-dashboard';
+export class AppComponent implements OnInit {
+    public title = 'microfront-dashboard';
+    public heroes: HeroModel[] = [];
+    public selectedHero: HeroModel;
+
+    public ngOnInit() {
+        this.heroes = (listOfHeros || []).map((item) => new HeroModel(item));
+    }
+
+    public handleClick(hero) {
+        this.selectedHero = hero;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeroModule} from 'microfront-cli/src/app/modules/hero/hero.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HeroModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
